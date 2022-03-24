a55228a09026c8e6cf95fc80ca32afdd41054cab
Paulo-Jimenez <75323560+Paulo-Jimenez@users.noreply.github.com>
2022-03-24 09:42:24 +0800

    Remove target attribute in Webdriver Uni contact us

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,9 @@
 
 describe("Test Contact Us form via WebdriverUni" ,()=>{
     it("Should be able to submit a successful submission via contact us form", ()=>{
-        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get("#contact-us").invoke('removeAttr', 'target').click({force: true})
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
@@ -15,7 +17,9 @@ describe("Test Contact Us form via WebdriverUni" ,()=>{
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", ()=>{
-        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get("#contact-us").invoke('removeAttr', 'target').click({force: true})
         cy.get('[name="first_name"]').type('Tom')
         cy.get('[name="last_name"]').type('Blogs')
         cy.get('textarea.feedback-input').type('This is a comment')
