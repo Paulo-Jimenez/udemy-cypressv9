d7578ee36866166175308c5402ea253e7e7d9963
Paulo-Jimenez <75323560+Paulo-Jimenez@users.noreply.github.com>
2022-03-26 10:26:44 +0800

    Cypress alerts and iframes

---
+++ cypress/integration/webdriver-uni/iframe.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe("Handling IFrame & Modals", () => {
+    it("Handle webdriveruni iframe and modal", () => {
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get("#iframe").invoke('removeAttr', 'target').click({ force: true })
+
+        cy.get('#frame').then($iframe => {
+            const body = $iframe.contents().find('body')
+            cy.wrap(body).as('iframe')
+        })
+
+        cy.get('@iframe').find('#button-find-out-more').click()
+
+        cy.get('@iframe').find('#myModal').as('modal')
+
+        cy.get('@modal').should(($expectedText) => {
+            const text = $expectedText.text()
+            expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods such as laptops, game consoles, cameras...')
+        })
+
+        cy.get('@modal').contains('Close').click()
+    })
+})
