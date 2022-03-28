d6538cf300cc61a1b6c348b486513f1c881d0010
Paulo-Jimenez <75323560+Paulo-Jimenez@users.noreply.github.com>
2022-03-28 09:50:13 +0800

    Cypress fixtures and custom commands

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
 describe("Iterate over elements", () => {
-    it("Log information of all hair care products", () => {
+    beforeEach(() => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='=product/category&path=']").contains("Hair Care").click()
-
+    })
+    it("Log information of all hair care products", () => {
+        
         cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
             cy.log("Index: " + index + " : " + $el.text())
             cy.log($list)
@@ -12,13 +14,16 @@ describe("Iterate over elements", () => {
     });
 
     it("Add specific product to basket", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='=product/category&path=']").contains("Hair Care").click()
 
-        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
-            if($el.text().includes("Eau Parfumee au The Vert Shampoo")) {
-                cy.wrap($el).click()
-            }
-        })
+        cy.selectProduct('Eau Parfumee au The Vert Shampoo')
     });
+
+    it("Add another specific product to basket", () => {
+        cy.selectProduct('Seaweed Conditioner')
+    });
+
+    it("Add another specific product to basket", () => {
+        cy.selectProduct('Pantene Pro-V Conditioner, Classic Care')
+    });
+
 })
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -14,22 +14,26 @@ describe("Test Contact Us form via WebdriverUni" ,()=>{
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
-        cy.get('[name="first_name"]').type(data.first_name)
-        cy.get('[name="last_name"]').type(data.last_name)
-        cy.get('[name="email"]').type(data.email)
-        cy.get('textarea.feedback-input').type('This is a comment')
-        cy.get('[type="submit"]').click()
-        cy.get('div#contact_reply > h1').should('have.text', 'Thank You for your Message!')
+        // cy.get('[name="first_name"]').type(data.first_name)
+        // cy.get('[name="last_name"]').type(data.last_name)
+        // cy.get('[name="email"]').type(data.email)
+        // cy.get('textarea.feedback-input').type('This is a comment')
+        // cy.get('[type="submit"]').click()
+        // cy.get('div#contact_reply > h1').should('have.text', 'Thank You for your Message!')
+
+        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, data.email, 'This is a comment', 'div#contact_reply > h1', 'Thank You for your Message!');
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", ()=>{
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
         cy.get("#contact-us").invoke('removeAttr', 'target').click({force: true})
-        cy.get('[name="first_name"]').type(data.first_name)
-        cy.get('[name="last_name"]').type(data.last_name)
-        cy.get('textarea.feedback-input').type('This is a comment')
-        cy.get('[type="submit"]').click()
-        cy.get('body').contains('Error: all fields are required')
+        // cy.get('[name="first_name"]').type(data.first_name)
+        // cy.get('[name="last_name"]').type(data.last_name)
+        // cy.get('textarea.feedback-input').type('This is a comment')
+        // cy.get('[type="submit"]').click()
+        // cy.get('body').contains('Error: all fields are required')
+
+        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, " ", 'This is a comment', 'body', 'Error: Invalid email address');
     });
 })
