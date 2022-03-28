3455d9a1a16f8afc91f9426c4a17a1c87a9f94e9
Paulo-Jimenez <75323560+Paulo-Jimenez@users.noreply.github.com>
2022-03-28 08:33:58 +0800

    Cypress fixtures

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,6 +1,12 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni" ,()=>{
+    before(function() {
+        cy.fixture('example').then(function(data) {
+            // this.data = data;
+            globalThis.data = data;
+        })
+    })
     it("Should be able to submit a successful submission via contact us form", ()=>{
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
@@ -8,9 +14,9 @@ describe("Test Contact Us form via WebdriverUni" ,()=>{
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
-        cy.get('[name="first_name"]').type('Joe')
-        cy.get('[name="last_name"]').type('Manganello')
-        cy.get('[name="email"]').type('joe.manganello@test.com')
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
+        cy.get('[name="email"]').type(data.email)
         cy.get('textarea.feedback-input').type('This is a comment')
         cy.get('[type="submit"]').click()
         cy.get('div#contact_reply > h1').should('have.text', 'Thank You for your Message!')
@@ -20,8 +26,8 @@ describe("Test Contact Us form via WebdriverUni" ,()=>{
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
         cy.get("#contact-us").invoke('removeAttr', 'target').click({force: true})
-        cy.get('[name="first_name"]').type('Tom')
-        cy.get('[name="last_name"]').type('Blogs')
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
         cy.get('textarea.feedback-input').type('This is a comment')
         cy.get('[type="submit"]').click()
         cy.get('body').contains('Error: all fields are required')
